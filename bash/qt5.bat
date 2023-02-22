473ac8dedfba894a1395ad5c9b74549b23e26a7c
FangYi <62888542+kikkimo@users.noreply.github.com>
2023-02-22 20:33:33 +0800

    Update qt5.bat

--- bash/qt5.bat
+++ bash/qt5.bat
@@ -1,2 +1,1 @@
-vcpkg remove protobuf
 vcpkg install qt5 qt5[3d,activeqt,charts,connectivity,datavis3d,declarative,doc,essentials,extras,graphicaleffects,imageformats,latest,location,mqtt,multimedia,networkauth,purchasing,quickcontrols,quickcontrols2,remoteobjects,script,scxml,sensors,serialbus,serialport,speech,svg,tools,translations,virtualkeyboard,webchannel,webengine,webglplugin,websockets,webview,xmlpatterns]  --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/buildtrees  --recurse  --editable
