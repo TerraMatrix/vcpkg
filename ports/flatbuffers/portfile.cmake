c080d752d7c2566b98caaad9299b6e38f56dd854
FangYi <62888542+kikkimo@users.noreply.github.com>
2023-02-23 15:50:05 +0800

    将flatbuffer降级到1.12.0#2版本
    将flatbuffer降级到1.12.0#2版本

--- ports/flatbuffers/portfile.cmake
+++ ports/flatbuffers/portfile.cmake
@@ -3,45 +3,48 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/flatbuffers
-    REF "v${VERSION}"
-    SHA512 fa62188f773ad044644a58caf1e25bef417dfdea47c9da8a2ea7f997154b4f3976019e32e73cc533696a3d4e45ec4a8402b6df140878dfa2ff078740d61b4b0f
+    REF v1.12.0
+    SHA512 8a0b88d739fa4694a69d3630140fe89fdd70d50bba4dadd1758d9aa2920cda16700bcafb8d89fe2a09ac907d3f378240c3cb4abc7106318136799836aba4b063
     HEAD_REF master
     PATCHES
+        ignore_use_of_cmake_toolchain_file.patch
+        no-werror.patch
         fix-uwp-build.patch
+        fix-issue-6036.patch # this patch is already applied to the latest master branch. 
+                             # remove it in next version update
 )
 
-set(options "")
-if(VCPKG_CROSSCOMPILING)
-    list(APPEND options -DFLATBUFFERS_BUILD_FLATC=OFF -DFLATBUFFERS_BUILD_FLATHASH=OFF)
-    if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
-        # The option may cause "#error Unsupported architecture"
-        list(APPEND options -DFLATBUFFERS_OSX_BUILD_UNIVERSAL=OFF)
-    endif()
+set(OPTIONS)
+if(VCPKG_TARGET_IS_UWP OR VCPKG_TARGET_IS_IOS)
+    list(APPEND OPTIONS -DFLATBUFFERS_BUILD_FLATC=OFF -DFLATBUFFERS_BUILD_FLATHASH=OFF)
 endif()
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS
         -DFLATBUFFERS_BUILD_TESTS=OFF
         -DFLATBUFFERS_BUILD_GRPCTEST=OFF
-        ${options}
+        ${OPTIONS}
 )
 
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/flatbuffers)
-vcpkg_fixup_pkgconfig()
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/flatbuffers)
 
 file(GLOB flatc_path ${CURRENT_PACKAGES_DIR}/bin/flatc*)
 if(flatc_path)
-    vcpkg_copy_tools(TOOL_NAMES flatc AUTO_CLEAN)
-else()
-    file(APPEND "${CURRENT_PACKAGES_DIR}/share/flatbuffers/Flatbuffers-config.cmake"
-"include(\"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/share/flatbuffers/FlatcTargets.cmake\")\n")
+    make_directory(${CURRENT_PACKAGES_DIR}/tools/flatbuffers)
+    get_filename_component(flatc_executable ${flatc_path} NAME)
+    file(
+        RENAME
+        ${flatc_path}
+        ${CURRENT_PACKAGES_DIR}/tools/flatbuffers/${flatc_executable}
+    )
+    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/flatbuffers)
 endif()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 
 # Handle copyright
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
