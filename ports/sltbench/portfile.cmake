e8448064c7831df29f27fbc9eb5e43f0c2512489
WIN-N5SUDEL61VO\Administrator <fywhu@qq.com>
2023-08-12 06:41:56 +0800

    修复sltbench

--- ports/sltbench/portfile.cmake
+++ ports/sltbench/portfile.cmake
@@ -5,15 +5,19 @@ vcpkg_from_github(
   REPO ivafanas/sltbench
   REF 52c7c7852abc3159185eb79e699ad77fadfc35bd
   SHA512 0c66b51f5a950a09df47019775941554538bc3642788f61aaf8c5ec3644d5fef721391f73c3fddfd9529159f9b81c7d7ed76c7995a79f37adaf8d0ff55a99d4b
-  HEAD_REF master)
+  HEAD_REF master
+  PATCHES
+      BenchmarksContainer.patch
+      disable_warnning_as_error.patch
+  )
 
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
-vcpkg_install_cmake()
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" WINDOWS_USE_MSBUILD)
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake/ TARGET_PATH share/sltbench)
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(
-  INSTALL ${SOURCE_PATH}/LICENSE
-  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
   RENAME copyright)
