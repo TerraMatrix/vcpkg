6cc38d66bcb071dd96514f66535a8c4cef099017
WIN-N5SUDEL61VO\Administrator <fywhu@qq.com>
2023-08-12 16:17:38 +0800

    修改mcc-lidar，去掉cuda特性

--- bash/mcc-lidar.bat
+++ bash/mcc-lidar.bat
@@ -1,1 +1,1 @@
-vcpkg install mcc-lidar[cuda] --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads  --recurse
+vcpkg install mcc-lidar --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads  --recurse
