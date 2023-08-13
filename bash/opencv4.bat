30b4cf1ca544a820163711519a48d888207d5c32
WIN-N5SUDEL61VO\Administrator <fywhu@qq.com>
2023-08-13 15:41:13 +0800

    修改opencv的编译项

--- bash/opencv4.bat
+++ bash/opencv4.bat
@@ -1,1 +1,3 @@
-vcpkg install opencv4[default-features,ade,contrib,dnn,eigen,halide,jasper,jpeg,lapack,nonfree,opengl,openmp,png,quirc,sfm,tbb,tiff] --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads --editable
+echo "opencv[halide]“¿¿µllvm, ƒø«∞±‡“Îllvm ß∞‹, »•µÙ¡Àhalide"
+vcpkg install opencv4[core,ade,contrib,dnn,eigen,jasper,jpeg,lapack,nonfree,opengl,openmp,png,quirc,sfm,tbb,tiff] --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads --editable
+
