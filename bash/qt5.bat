4fefc9ca1a72e16a2ebd9d37eb9ad7a720603820
FangYi <62888542+kikkimo@users.noreply.github.com>
2024-03-11 23:05:12 +0800

    [qt5]: 修改Qt5的bat文件

--- bash/qt5.bat
+++ bash/qt5.bat
@@ -1,1 +1,8 @@
-vcpkg install qt5 qt5[3d,activeqt,charts,connectivity,datavis3d,declarative,doc,essentials,extras,graphicaleffects,imageformats,latest,location,mqtt,multimedia,networkauth,purchasing,quickcontrols,quickcontrols2,remoteobjects,script,scxml,sensors,serialbus,serialport,speech,svg,tools,translations,virtualkeyboard,webchannel,webengine,webglplugin,websockets,webview,xmlpatterns]  --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads  --editable
+vcpkg install qt5 qt5[3d,activeqt,charts,connectivity,datavis3d,declarative,doc,essentials,extras,graphicaleffects,imageformats,latest,location,mqtt,multimedia,networkauth,purchasing,quickcontrols,quickcontrols2,remoteobjects,script,scxml,sensors,serialbus,serialport,speech,svg,tools,translations,virtualkeyboard,webchannel,webengine,webglplugin,websockets,webview,xmlpatterns]  --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads  --editable  --debug --allow-unsupported
+
+
+vcpkg install qt5[webengine]  --triplet=x64-windows --x-buildtrees-root=F:/a  --downloads-root=F:/vcpkg/downloads  --debug
+
+
+vcpkg install qt5 qt5[3d,activeqt,charts,connectivity,datavis3d,declarative,doc,essentials,extras,graphicaleffects,imageformats,latest,location,mqtt,multimedia,networkauth,purchasing,quickcontrols,quickcontrols2,remoteobjects,script,scxml,sensors,serialbus,serialport,speech,svg,tools,translations,virtualkeyboard,webchannel,webglplugin,websockets,webview,xmlpatterns]  --triplet=x64-windows --x-buildtrees-root=F:/vcpkg/vcpkg-win7/buildtrees  --downloads-root=F:/vcpkg/downloads  --editable  --debug --allow-unsupported
+
