074b737d616b6d3607f400bb2e1241b1b1036062
fuhaolin <480473516@qq.com>
2025-10-13 16:55:46 +0800

    [ceres] ceres版本固定到2.1.0#5

---
+++ bash/ceres.bat
@@ -0,0 +1,1 @@
+vcpkg install ceres[core,cxsparse,eigensparse,lapack,suitesparse,tools] --triplet=x64-windows --x-buildtrees-root=E:/vcpkg/buildtrees  --downloads-root=E:/vcpkg/downloads   --recurse
