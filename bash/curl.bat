7ab694af2ce004b685ce79c602afd9c6f2fb6f6e
fuhaolin <480473516@qq.com>
2025-10-13 16:56:56 +0800

    [curl] curl版本固定到8.11.0#1

---
+++ bash/curl.bat
@@ -0,0 +1,1 @@
+vcpkg install curl[core,brotli,http2,non-http,schannel,ssl,ssh,sspi,winssl] --triplet=x64-windows --x-buildtrees-root=E:/vcpkg/buildtrees  --downloads-root=E:/vcpkg/downloads    --recurse 
