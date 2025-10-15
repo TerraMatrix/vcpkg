d7e1e9187098bd0b381cf1c06732aed37da97025
fuhaolin <480473516@qq.com>
2025-10-15 12:16:55 +0800

    [pdal] pdal版本固定到2.8.1

---
+++ installed/x64-windows/share/pdal/PDALTargets-debug.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pdalcpp" for configuration "Debug"
+set_property(TARGET pdalcpp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(pdalcpp PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/pdalcpp.lib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "GDAL::GDAL;PROJ::proj;geotiff_library;nlohmann_json_schema_validator::validator"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/pdalcpp.dll"
+  )
+
+list(APPEND _cmake_import_check_targets pdalcpp )
+list(APPEND _cmake_import_check_files_for_pdalcpp "${_IMPORT_PREFIX}/debug/lib/pdalcpp.lib" "${_IMPORT_PREFIX}/debug/bin/pdalcpp.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
