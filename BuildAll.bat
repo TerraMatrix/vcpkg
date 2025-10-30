49e336aa0b7fb39aa2941651d703e8e52d8fd730
fuhaolin <480473516@qq.com>
2025-10-30 09:31:50 +0800

    [qgis] qgis版本固定到3.30.2,并添加BuildAll脚本

--- BuildAll.bat
+++ BuildAll.bat
@@ -7,43 +7,43 @@ set http_proxy=http://127.0.0.1:7890 & set https_proxy=http://127.0.0.1:7890
 echo "set -install-root  and  -packages-root"
 vcpkg --x-install-root=./installed --x-packages-root=./packages  --vcpkg-root=./
 
-echo "install  boost 1.81.0"
+echo "install  boost 1.86.0"
 echo "--------------------------------------------"
 call ./bash/boost.bat
 
-echo "install  fmt 9.1.0#1"
+echo "install  fmt 11.0.2#1"
 echo "--------------------------------------------"
 call ./bash/fmt.bat
 
-echo "install  sltbench  2.4.0"
+echo "install  sltbench  2.4.0#3"
 echo "--------------------------------------------"
 call ./bash/sltbench .bat
 
-echo "install  freetype 2.12.1#3"
+echo "install  freetype 2.13.3"
 echo "--------------------------------------------"
 call ./bash/freetype.bat
 
-echo "install  hdf5 1.12.2#6"
+echo "install  hdf5 1.14.4.3#3"
 echo "--------------------------------------------"
 call ./bash/hdf5.bat
 
-echo "install  flann 2019-04-07#6"
+echo "install  flann 2019-04-07#7"
 echo "--------------------------------------------"
 call ./bash/flann.bat
 
-echo "install  glm 0.9.9.8#2"
+echo "install  glm 1.0.1#3"
 echo "--------------------------------------------"
 call ./bash/glm.bat
 
-echo "install  gtest 1.12.1#1"
+echo "install  gtest 1.15.2"
 echo "--------------------------------------------"
 call ./bash/gtest.bat
 
-echo "install  glew  2.2.0"
+echo "install  glew  2.2.0#4"
 echo "--------------------------------------------"
 call ./bash/glew.bat
 
-echo "install  gmp  6.2.1#15"
+echo "install  gmp  6.3.0#1"
 echo "--------------------------------------------"
 call ./bash/gmp.bat
 
@@ -51,39 +51,39 @@ echo "install  cereal  1.3.2#1"
 echo "--------------------------------------------"
 call ./bash/cereal.bat
 
-echo "install  gsl  2.7.1#1"
+echo "install  gsl  2.8"
 echo "--------------------------------------------"
 call ./bash/gsl.bat
 
-echo "install  openni2  2.2.0.33#14"
+echo "install  openni2  2.2.0.33#15"
 echo "--------------------------------------------"
 call ./bash/openni2.bat
 
-echo "install  openexr  3.1.5"
+echo "install  openexr  3.3.1"
 echo "--------------------------------------------"
 call ./bash/openexr.bat
 
-echo "install  pybind11  2.10.0#1"
+echo "install  pybind11  2.13.6"
 echo "--------------------------------------------"
 call ./bash/pybind11.bat
 
-echo "install  ceres   2.1.0#3"
+echo "install  ceres   2.1.0#5"
 echo "--------------------------------------------"
 call ./bash/ceres.bat
 
-echo "install  curl  7.87.0#1"
+echo "install  curl  8.11.0#1"
 echo "--------------------------------------------"
 call ./bash/curl.bat
 
-echo "install  proj4  8.9.9#1"
+echo "install  proj  9.5.0"
 echo "--------------------------------------------"
-call ./bash/proj4.bat
+call ./bash/proj.bat
 
-echo "install  libgeotiff 1.7.1#2"
+echo "install  libgeotiff 1.7.3#1"
 echo "--------------------------------------------"
 call ./bash/libgeotiff.bat
 
-echo "install  liblas  1.8.1#11"
+echo "install  liblas  1.8.1#15"
 echo "--------------------------------------------"
 call ./bash/liblas.bat
 
@@ -95,43 +95,43 @@ echo "install  crunch  104"
 echo "--------------------------------------------"
 call ./bash/crunch.bat
 
-echo "install  libkml  1.3.0#11"
+echo "install  libkml  1.3.0#13"
 echo "--------------------------------------------"
 call ./bash/libkml.bat
 
-echo "install  xerces-c 3.2.4#2"
+echo "install  xerces-c 3.2.5"
 echo "--------------------------------------------"
 call ./bash/xerces-c.bat
 
-echo "install  libxml2 2.10.3"
+echo "install  libxml2 2.11.9"
 echo "--------------------------------------------"
 call ./bash/libxml2.bat
 
-echo "install  libpq  14.4#3"
+echo "install  libpq  16.4"
 echo "--------------------------------------------"
 call ./bash/libpq.bat
 
-echo "install  lastools  2020-05-09#1"
+echo "install  lastools  2.0.3"
 echo "--------------------------------------------"
 call ./bash/lastools.bat
 
-echo "install  poppler  23.1.0#1"
+echo "install  poppler  24.3.0#1"
 echo "--------------------------------------------"
 call ./bash/poppler.bat
 
-echo "install  gdal  3.6.3#1"
+echo "install  gdal  3.10.0"
 echo "--------------------------------------------"
 call ./bash/gdal.bat
 
-echo "install  pdal  2.4.3#1"
+echo "install  pdal  2.5.3#1"
 echo "--------------------------------------------"
 call ./bash/pdal.bat
 
-echo "install  Qt5  5.15.8"
+echo "install  Qt5  5.15.15"
 echo "--------------------------------------------"
 call ./bash/qt5.bat
 
-echo "install  glib  2.75.3"
+echo "install  glib  2.80.0#1"
 echo "--------------------------------------------"
 call ./bash/glib.bat
 
@@ -139,27 +139,27 @@ echo "install  ilmbase 3"
 echo "--------------------------------------------"
 call ./bash/ilmbase.bat
 
-echo "install  fontconfig  2.14.1#1"
+echo "install  fontconfig  2.15.0#1"
 echo "--------------------------------------------"
 call ./bash/fontconfig.bat
 
-echo "install  ffmpeg  5.1.2#4"
+echo "install  ffmpeg  7.1#1"
 echo "--------------------------------------------"
 call ./bash/ffmpeg.bat
 
-echo "install  cgal  5.5.1#4"
+echo "install  cgal  6.0.1"
 echo "--------------------------------------------"
 call ./bash/cgal.bat
 
-echo "install  pcl  1.13.0"
+echo "install  pcl  1.14.1#3"
 echo "--------------------------------------------"
 call ./bash/pcl.bat
 
-echo "install  opencv4  4.7.0#2"
+echo "install  opencv4  4.8.0#14"
 echo "--------------------------------------------"
 call ./bash/opencv4.bat
 
-echo "install  osg  3.6.5#18"
+echo "install  osg  3.6.5#25"
 echo "--------------------------------------------"
 call ./bash/osg.bat
 
@@ -167,23 +167,39 @@ echo "install  osg-qt  Qt5#3"
 echo "--------------------------------------------"
 call ./bash/osg-qt.bat
 
-echo "install  leveldb  1.22#6"
+echo "install  leveldb  1.23"
 echo "--------------------------------------------"
 call ./bash/leveldb.bat
 
-echo "install  grpc 1.51#1"
+echo "install  grpc 1.51.1"
 echo "--------------------------------------------"
 call ./bash/grpc.bat
 
-echo "install  shapelib 1.5.0#3"
+echo "install  shapelib 1.6.1"
 echo "--------------------------------------------"
 call ./bash/shapelib.bat
 
-echo "install  vcglib 2022.02"
+echo "install  vcglib 2023.12"
 echo "--------------------------------------------"
 call ./bash/vcglib.bat
 
-echo "install  qgis"
+echo "install  pugixml 1.14"
+echo "--------------------------------------------"
+call ./bash/pugixml.bat
+
+echo "install  nlohmann-json 3.11.3#1"
+echo "--------------------------------------------"
+call ./bash/nlohmann-json.bat
+
+echo "install  highway 1.2.0"
+echo "--------------------------------------------"
+call ./bash/highway.bat
+
+echo "install  skia 129#2"
+echo "--------------------------------------------"
+call ./bash/skia.bat
+
+echo "install  qgis 3.30.2"
 echo "--------------------------------------------"
 call ./bash/qgis.bat
 
