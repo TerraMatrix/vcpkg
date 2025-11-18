d9a614747058f80dad48fbcb63ab2e40af3e77a4
Fuhaolin <107407394+Fuczy@users.noreply.github.com>
2025-11-18 10:52:18 +0800

    [chore] 运行buildall脚本进行第一次编译

--- bash/mcc-lidar.sh
+++ bash/mcc-lidar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Converted from mcc-lidar.bat for Linux compatibility
-./vcpkg install mcc-lidar --triplet=arm64-linux --x-buildtrees-root=/data/workspace/vcpkg/buildtrees  --downloads-root=/data/workspace/vcpkg/downloads  --recurse
+./vcpkg install mcc-lidar[cuda] --triplet=arm64-linux --x-buildtrees-root=/data/workspace/vcpkg/buildtrees  --downloads-root=/data/workspace/vcpkg/downloads  --recurse
