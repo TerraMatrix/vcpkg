458e82812e97f9a8c84b19e01d3d42cd32cda5fe
Fuhaolin <107407394+Fuczy@users.noreply.github.com>
2025-12-11 12:30:10 +0800

    [qt5] qt5版本固定到5.15.17

---
+++ installed/arm64-linux/share/cmake/Qt5VirtualKeyboard/Qt5VirtualKeyboard_QtVirtualKeyboardPinyinPlugin.cmake
@@ -0,0 +1,93 @@
+
+add_library(Qt5::QtVirtualKeyboardPinyinPlugin MODULE IMPORTED)
+
+set(_Qt5QtVirtualKeyboardPinyinPlugin_MODULE_DEPENDENCIES "VirtualKeyboard;Gui;Qml;Core")
+
+foreach(_module_dep ${_Qt5QtVirtualKeyboardPinyinPlugin_MODULE_DEPENDENCIES})
+    if(NOT Qt5${_module_dep}_FOUND)
+        find_package(Qt5${_module_dep}
+            1.0.0 ${_Qt5VirtualKeyboard_FIND_VERSION_EXACT}
+            ${_Qt5VirtualKeyboard_DEPENDENCIES_FIND_QUIET}
+            ${_Qt5VirtualKeyboard_FIND_DEPENDENCIES_REQUIRED}
+            PATHS "${CMAKE_CURRENT_LIST_DIR}/.." NO_DEFAULT_PATH
+        )
+    endif()
+endforeach()
+
+if(EXISTS "${_qt5VirtualKeyboard_install_prefix}/plugins/virtualkeyboard/libqtvirtualkeyboard_pinyin.prl")
+    _qt5_VirtualKeyboard_process_prl_file(
+        "${_qt5VirtualKeyboard_install_prefix}/plugins/virtualkeyboard/libqtvirtualkeyboard_pinyin.prl" RELEASE
+        _Qt5QtVirtualKeyboardPinyinPlugin_STATIC_RELEASE_LIB_DEPENDENCIES
+        _Qt5QtVirtualKeyboardPinyinPlugin_STATIC_RELEASE_LINK_FLAGS
+    )
+endif()
+
+if(EXISTS "${_qt5VirtualKeyboard_install_prefix}/debug/plugins/virtualkeyboard/libqtvirtualkeyboard_pinyin.prl")
+    _qt5_VirtualKeyboard_process_prl_file(
+        "${_qt5VirtualKeyboard_install_prefix}/debug/plugins/virtualkeyboard/libqtvirtualkeyboard_pinyin.prl" DEBUG
+        _Qt5QtVirtualKeyboardPinyinPlugin_STATIC_DEBUG_LIB_DEPENDENCIES
+        _Qt5QtVirtualKeyboardPinyinPlugin_STATIC_DEBUG_LINK_FLAGS
+    )
+endif()
+
+set_property(TARGET Qt5::QtVirtualKeyboardPinyinPlugin PROPERTY INTERFACE_SOURCES
+    "${CMAKE_CURRENT_LIST_DIR}/Qt5VirtualKeyboard_QtVirtualKeyboardPinyinPlugin_Import.cpp"
+)
+
+if(EXISTS "${_qt5VirtualKeyboard_install_prefix}/plugins/virtualkeyboard/libqtvirtualkeyboard_pinyin.a")
+    _populate_VirtualKeyboard_plugin_properties(QtVirtualKeyboardPinyinPlugin RELEASE "virtualkeyboard/libqtvirtualkeyboard_pinyin.a" FALSE)
+endif()
+if(EXISTS "${_qt5VirtualKeyboard_install_prefix}/debug/plugins/virtualkeyboard/libqtvirtualkeyboard_pinyin.a")
+    _populate_VirtualKeyboard_plugin_properties(QtVirtualKeyboardPinyinPlugin DEBUG "virtualkeyboard/libqtvirtualkeyboard_pinyin.a" FALSE)
+endif()
+
+list(APPEND Qt5VirtualKeyboard_PLUGINS Qt5::QtVirtualKeyboardPinyinPlugin)
+set_property(TARGET Qt5::VirtualKeyboard APPEND PROPERTY QT_ALL_PLUGINS_virtualkeyboard Qt5::QtVirtualKeyboardPinyinPlugin)
+# $<GENEX_EVAL:...> wasn't added until CMake 3.12, so put a version guard around it
+if(CMAKE_VERSION VERSION_LESS "3.12")
+    set(_manual_plugins_genex "$<TARGET_PROPERTY:QT_PLUGINS>")
+    set(_plugin_type_genex "$<TARGET_PROPERTY:QT_PLUGINS_virtualkeyboard>")
+    set(_no_plugins_genex "$<TARGET_PROPERTY:QT_NO_PLUGINS>")
+else()
+    set(_manual_plugins_genex "$<GENEX_EVAL:$<TARGET_PROPERTY:QT_PLUGINS>>")
+    set(_plugin_type_genex "$<GENEX_EVAL:$<TARGET_PROPERTY:QT_PLUGINS_virtualkeyboard>>")
+    set(_no_plugins_genex "$<GENEX_EVAL:$<TARGET_PROPERTY:QT_NO_PLUGINS>>")
+endif()
+set(_user_specified_genex
+    "$<IN_LIST:Qt5::QtVirtualKeyboardPinyinPlugin,${_manual_plugins_genex};${_plugin_type_genex}>"
+)
+set(_user_specified_genex_versionless
+    "$<IN_LIST:Qt::QtVirtualKeyboardPinyinPlugin,${_manual_plugins_genex};${_plugin_type_genex}>"
+)
+string(CONCAT _plugin_genex
+    "$<$<OR:"
+        # Add this plugin if it's in the list of manually specified plugins or in the list of
+        # explicitly included plugin types.
+        "${_user_specified_genex},"
+        "${_user_specified_genex_versionless},"
+        # Add this plugin if all of the following are true:
+        # 1) the list of explicitly included plugin types is empty
+        # 2) the QT_PLUGIN_EXTENDS property for the plugin is empty or equal to one of the modules
+        #    listed in the plugin's PLUGIN_EXTEND qmake variable
+        # 3) the user hasn't explicitly excluded the plugin.
+        # TODO: Note that the current implementation of (2) is not entirely correct QTBUG-93501
+        "$<AND:"
+            "$<STREQUAL:${_plugin_type_genex},>,"
+            "$<OR:"
+                
+                "$<STREQUAL:$<TARGET_PROPERTY:Qt5::QtVirtualKeyboardPinyinPlugin,QT_PLUGIN_EXTENDS>,>"
+            ">,"
+            "$<NOT:$<IN_LIST:Qt5::QtVirtualKeyboardPinyinPlugin,${_no_plugins_genex}>>,"
+            "$<NOT:$<IN_LIST:Qt::QtVirtualKeyboardPinyinPlugin,${_no_plugins_genex}>>"
+        ">"
+    ">:Qt5::QtVirtualKeyboardPinyinPlugin>"
+)
+set_property(TARGET Qt5::VirtualKeyboard APPEND PROPERTY INTERFACE_LINK_LIBRARIES
+    ${_plugin_genex}
+)
+set_property(TARGET Qt5::QtVirtualKeyboardPinyinPlugin APPEND PROPERTY INTERFACE_LINK_LIBRARIES
+    "Qt5::VirtualKeyboard;Qt5::Gui;Qt5::Qml;Qt5::Core"
+)
+set_property(TARGET Qt5::QtVirtualKeyboardPinyinPlugin PROPERTY QT_PLUGIN_TYPE "virtualkeyboard")
+set_property(TARGET Qt5::QtVirtualKeyboardPinyinPlugin PROPERTY QT_PLUGIN_EXTENDS "")
+set_property(TARGET Qt5::QtVirtualKeyboardPinyinPlugin PROPERTY QT_PLUGIN_CLASS_NAME "QtVirtualKeyboardPinyinPlugin")
