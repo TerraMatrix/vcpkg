2c4ec48f863b4f0ebf044b7745b26e6a9ac589dc
Fuhaolin <107407394+Fuczy@users.noreply.github.com>
2025-12-30 14:36:36 +0800

    [opencv4] opencv4版本固定到4.11.0

--- bash/opencv4.sh
+++ bash/opencv4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Converted from opencv4.bat for Linux compatibility
-./vcpkg install opencv4[core,ade,contrib,dnn,eigen,highgui,jpegxl,openvino,openjpeg,jpeg,nonfree,opengl,openmp,png,quirc,sfm,tbb,tiff] --triplet=arm64-linux --x-buildtrees-root=/data/workspace/vcpkg/buildtrees  --downloads-root=/data/workspace/vcpkg/downloads --recurse --editable --allow-unsupported --keep-going
+./vcpkg install opencv4[core,ade,contrib,dnn,eigen,highgui,gtk,jpegxl,openvino,openjpeg,jpeg,nonfree,opengl,openmp,png,quirc,sfm,tbb,tiff] --triplet=arm64-linux --x-buildtrees-root=/data/workspace/vcpkg/buildtrees  --downloads-root=/data/workspace/vcpkg/downloads --recurse --editable --allow-unsupported --keep-going
